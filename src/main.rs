bcc5369ba104d0c4e38247c9b772b7b8405838aa
k-juyeong <wndud8532@naver.com>
2023-01-30 16:48:56 +0900

    Example1 end

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    // pc random choice
+
+    handle.read_line(&mut buffer)?;
+
+    buffer = (buffer.trim_end()).to_string();
+
+    match buffer.as_str() {
+        "rock" => println!("pc: paper"),
+        "paper" => println!("pc: scissors"),
+        "scissors" => println!("pc: rock"),
+        _ => panic!("We're playing rock-paper-scissor right now"),
+    }
+
+    Ok(())
 }
